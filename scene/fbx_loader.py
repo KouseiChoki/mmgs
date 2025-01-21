2bc36061452c127f29e99c7f3ea50dc8fe6a175e
root <qhong@pixelworks.com>
2025-01-21 07:00:22 +0000

    0121

--- scene/fbx_loader.py
+++ scene/fbx_loader.py
@@ -57,7 +57,12 @@ def fetchPly(path):
     vertices = plydata['vertex']
     positions = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
     colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
-    normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
+    # normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
+    normals = np.vstack([
+    vertices['nx'] if 'nx' in vertices else vertices['normal_x'],
+    vertices['ny'] if 'ny' in vertices else vertices['normal_y'],
+    vertices['nz'] if 'nz' in vertices else vertices['normal_z']
+]).T
     return BasicPointCloud(points=positions, colors=colors, normals=normals)
 
 def cal_qvec(data):
